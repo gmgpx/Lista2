26cd342877839a385168b23264eec74a217c1dca
Gustavo Monteiro <158373467+gmgpx@users.noreply.github.com>
2024-02-29 19:09:07 -0300

    Update Main.java

--- Lista_condicionais/ex1/src/Main.java
+++ Lista_condicionais/ex1/src/Main.java
@@ -2,23 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Cria um scanner para ler a entrada do usuário
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite um número real: ");
-        // Lê o número real fornecido pelo usuário
-        double numero = scanner.nextDouble();
+        double n = scanner.nextDouble();
 
-        // Verifica se o número é positivo
         if (numero >= 0) {
-            // Calcula a raiz quadrada usando o método Math.sqrt
-            double raizQuadrada = Math.sqrt(numero);
-            System.out.println("A raiz quadrada de " + numero + " é " + raizQuadrada);
+            double raiz = Math.sqrt(numero);
+            System.out.println("A raiz quadrada de " + n + " é " + raiz);
         } else {
             System.out.println("Valor inválido!");
         }
 
-        // Fecha o scanner para liberar recursos
-        scanner.close();
     }
 }
